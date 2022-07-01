da960d2622bb209644d685e233535530452e206e
utkarshgubrelay2000 <utkarshgubrelay2000@gmail.com>
2022-07-02 00:18:36 +0530

    compling contracts

---
+++ compile.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+
+
+const helloPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(helloPath, 'UTF-8');
+
+var input = {
+    language: 'Solidity',
+    sources: {
+        'Inbox.sol' : {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': [ '*' ]
+            }
+        }
+    }
+}; 
+console.log(JSON.parse(solc.compile(JSON.stringify(input))));
